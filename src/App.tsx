37777f3da73cfe37af560eeeee81717fd04e3bad
fhcs <henrysilva14@gmail.com>
2024-09-27 21:05:24 -0300

    feat: add Header componet

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Header from './components/Header/Header'
+
 import './App.css'
 
 function App() {
 
   return (
-    <h2>Welcome to my personal page</h2>
+    <Header />
   )
 }
 
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,18 @@
+import './Header.css';
+
+export default function Header(){
+  return (
+    <div className='header'>
+      <nav>
+        <h2><a>FHCS</a></h2>
+        <ul>
+          <li><a href="">HOME</a></li>
+          <li><a href="">STACKS</a></li>
+          <li><a href="">PROJECTS</a></li>
+          <li><a href="">RESUME</a></li>
+          <li><a href="">CONTACT</a></li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
